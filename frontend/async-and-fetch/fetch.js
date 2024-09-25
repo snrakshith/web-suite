871ee929147c54e29726272584a813f956d1394b
snrakshith <snrakshith.97@gmail.com>
2024-09-25 21:50:01 +0530

    docs: fix typos in fetch

--- frontend/async-and-fetch/fetch.js
+++ frontend/async-and-fetch/fetch.js
@@ -30,11 +30,29 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     console.log(err);
   });
 
-getData();
-
+//  Using async & await
 async function getData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   //   console.log(response);
   const data = await response.json();
   console.log(data);
+}
+
+getData();
+
+// In case of react
+
+export default function FetchDataExample() {
+  const [data, setData] = (useState < null) | (any > []);
+  useEffect(() => {
+    fetch(`https://api.ikurehealthtech.com/dev/healthq/datasets`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+  if (isLoading) return <SplashScreen />;
+  return (
+    <>
+      <div>{data}</div>
+    </>
+  );
 }
